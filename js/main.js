33940c70a12ac67fc0f035f6f383de40e61b22ab
Klaudia <koliber96k@gmail.com>
2022-12-14 18:45:37 +0100

    29-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function multiply(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+console.log( multiply(3, 5) );
+
+let wynikSuma = multiply(10, 5);
+
+console.log(wynikSuma);
